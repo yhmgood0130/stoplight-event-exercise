4a7af9bb5e0cc22613d9afd67633e3436c1e9ced
Hyunmo <yhmgood0130@gmail.com>
2017-06-14 15:51:41 -0600

    Added light function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+  var stop = document.querySelector('#stopButton.button');
+  var slow = document.querySelector('#slowButton.button');
+  var go = document.querySelector('#goButton.button');
+  stop.addEventListener("click",function(){document.querySelector('#stopLight.bulb').classList.add("stop")});
+  slow.addEventListener("click",function(){document.querySelector('#slowLight.bulb').classList.add("slow")});
+  go.addEventListener("click",function(){document.querySelector('#goLight.bulb').classList.add("go")});
 })();
